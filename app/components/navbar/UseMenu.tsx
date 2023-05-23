08f3eabd746a73e9b743fb4a96ff824f11beb40c
davidbecker93 <trongvanle93@gmail.com>
2023-05-23 16:58:03 +0700

    first commit

---
+++ app/components/navbar/UseMenu.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+const UseMenu = () => {
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div onClick={() => { }} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">Airbnb your home</div>
+                <div className=""></div>
+            </div>
+        </div>
+    );
+}
+
+export default UseMenu;
