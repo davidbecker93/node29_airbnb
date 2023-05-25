aa61dc6364100e00ac672e66b7c69bcefdd9dbbb
davidbecker93 <trongvanle93@gmail.com>
2023-05-25 11:01:02 +0700

    secondary commit

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -57,15 +57,15 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, f
         <>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
                 <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+                    {/* CONTENT BOARD */}
                     <div className={`translate duration-300 h-full 
-                    ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                    ${showModal ? 'translate-y-6' : 'translate-y-full'}
                     ${showModal ? 'opacity-100' : 'opacity-0'}
                     `}>
-                        {/* CONTENT BOARD */}
                         <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                             {/* HEADER */}
                             <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
-                                <button onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9">
+                                <button onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute right-9">
                                     <IoMdClose size={18} />
                                 </button>
                                 <div className="text-lg font-semibold">
